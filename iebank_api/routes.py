df8fe1015687c10ecbe68ec06ef8098357e51ae2
youssef-gebaly <153581943+youssef-gebaly@users.noreply.github.com>
2023-12-12 06:46:10 +0100

    added country

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -26,7 +26,8 @@ def skull():
 def create_account():
     name = request.json['name']
     currency = request.json['currency']
-    account = Account(name, currency)
+    country = request.json['country']
+    account = Account(name, currency, country)
     db.session.add(account)
     db.session.commit()
     return format_account(account)
@@ -62,6 +63,7 @@ def format_account(account):
         'account_number': account.account_number,
         'balance': account.balance,
         'currency': account.currency,
+        'country': account.country,
         'status': account.status,
         'created_at': account.created_at
     }
